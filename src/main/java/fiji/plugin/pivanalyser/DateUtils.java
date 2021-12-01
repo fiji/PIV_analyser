018f6ef36f8002706414db022008aab303205c45
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2021-12-01 10:11:57 +0100

    Code style changes.

--- src/main/java/fiji/plugin/pivanalyser/DateUtils.java
+++ src/main/java/fiji/plugin/pivanalyser/DateUtils.java
@@ -1,18 +1,22 @@
 package fiji.plugin.pivanalyser;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class DateUtils {
-  public static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
+public class DateUtils
+{
+	public static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
 
-  public static String now() {
-    Calendar cal = Calendar.getInstance();
-    SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
-    return sdf.format(cal.getTime());
+	public static String now()
+	{
+		final Calendar cal = Calendar.getInstance();
+		final SimpleDateFormat sdf = new SimpleDateFormat( DATE_FORMAT_NOW );
+		return sdf.format( cal.getTime() );
 
-  }
+	}
 
-  public static void  main(String arg[]) {
-    System.out.println("Now : " + DateUtils.now());
-  }
+	public static void main( final String arg[] )
+	{
+		System.out.println( "Now : " + DateUtils.now() );
+	}
 }
